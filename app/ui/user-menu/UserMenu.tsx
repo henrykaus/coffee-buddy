e205a51c8b82a2355567171c6a76b41bc1d518ed
Henry Kaus <henrykaus19@gmail.com>
2025-04-23 22:11:55 -0700

    Updated UserMenu to close when clicked outside

--- app/ui/user-menu/UserMenu.tsx
+++ app/ui/user-menu/UserMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {EllipsisIcon, LogOutIcon} from '@/app/ui/icons';
 import {usePathname, useRouter, useSearchParams} from 'next/navigation';
 import {HomeActionType} from '@/app/lib/enums';
@@ -20,14 +20,36 @@ export default function UserMenu(props: UserMenuProps) {
   const pathname = usePathname();
   const {replace} = useRouter();
 
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [isMenuOpen]);
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  const handleClickOutside = (event: any) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      closeMenu();
+    }
+  };
+
   const handleMoreOptionsClick = () => {
+    closeMenu();
+
     const params = new URLSearchParams(searchParams);
     params.set('action', HomeActionType.MoreOptions);
     replace(`${pathname}?${params.toString()}`);
   };
 
   return (
-    <div className='relative text-slate-700 font-medium'>
+    <div className='relative text-slate-700 font-medium' ref={ref}>
       <button
         className='rounded-full p-1 transition hover:bg-slate-200/80'
         onClick={() => setIsMenuOpen(!isMenuOpen)}
