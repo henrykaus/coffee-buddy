afe4e1f92a1f4188e3c42ad7781afe845b20a9e4
Henry Kaus <henrykaus19@gmail.com>
2025-05-13 20:05:50 -0700

    Fixed user menu on mobile

--- app/ui/user-menu/UserMenu.tsx
+++ app/ui/user-menu/UserMenu.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React, {useRef} from 'react';
+import React, {useState} from 'react';
 import {EllipsisIcon, LogOutIcon} from '@/app/ui/icons';
 import {usePathname, useRouter, useSearchParams} from 'next/navigation';
 import {HomeActionType, Route} from '@/app/lib/enums';
 import {signOut} from 'next-auth/react';
 import {UserAvatar} from '@/app/ui/user-menu/UserAvatar';
+import useCloseableDropdown from '@/app/hooks/useCloseableDropdown';
 
 interface UserMenuProps {
   imageUrl?: string | null;
@@ -18,7 +19,9 @@ export default function UserMenu(props: UserMenuProps) {
   const pathname = usePathname();
   const {replace} = useRouter();
 
-  const ref = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const ref = useCloseableDropdown<HTMLDivElement>(() => setIsOpen(false));
 
   const handleMoreOptionsClick = () => {
     ref.current?.blur();
@@ -29,36 +32,36 @@ export default function UserMenu(props: UserMenuProps) {
   };
 
   return (
-    <div
-      className='relative text-slate-700 font-medium [&:focus-within>ul]:block'
-      ref={ref}
-    >
+    <div className='relative text-slate-700 font-medium' ref={ref}>
       <button
         aria-label='Open user menu'
         className='rounded-full p-1 transition hover:bg-slate-200/80'
+        onClick={() => setIsOpen(true)}
       >
         <UserAvatar imageUrl={imageUrl} />
       </button>
-      <ul className='bg-(--background) rounded-lg shadow-xl w-fit absolute hidden end-0 transition'>
-        <li className='hover:bg-slate-100 border-t-2 border-l-2 border-r-2 border-slate-300 rounded-t-lg'>
-          <button
-            className='flex justify-between p-2 gap-2 w-full whitespace-nowrap'
-            onClick={() => signOut({redirectTo: `/${Route.Login}`})}
-          >
-            <span>Sign Out</span>
-            <LogOutIcon />
-          </button>
-        </li>
-        <li className='hover:bg-slate-100 border-b-2 border-l-2 border-r-2 border-slate-300 rounded-b-lg'>
-          <button
-            className='flex justify-between p-2 gap-2 w-full whitespace-nowrap'
-            onClick={handleMoreOptionsClick}
-          >
-            <span>More Options</span>
-            <EllipsisIcon />
-          </button>
-        </li>
-      </ul>
+      {isOpen && (
+        <ul className='bg-(--background) rounded-lg shadow-xl w-fit absolute end-0 transition'>
+          <li className='hover:bg-slate-100 border-t-2 border-l-2 border-r-2 border-slate-300 rounded-t-lg'>
+            <button
+              className='flex justify-between p-2 gap-2 w-full whitespace-nowrap'
+              onClick={() => signOut({redirectTo: `/${Route.Login}`})}
+            >
+              <span>Sign Out</span>
+              <LogOutIcon />
+            </button>
+          </li>
+          <li className='hover:bg-slate-100 border-b-2 border-l-2 border-r-2 border-slate-300 rounded-b-lg'>
+            <button
+              className='flex justify-between p-2 gap-2 w-full whitespace-nowrap'
+              onClick={handleMoreOptionsClick}
+            >
+              <span>More Options</span>
+              <EllipsisIcon />
+            </button>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
