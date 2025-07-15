70bd386e3ea68c25baf6caf60d120f3b2e8456ab
Henry Kaus <henrykaus19@gmail.com>
2025-07-14 18:30:59 -0700

    Added loading.tsx file for updates page

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,13 +1,37 @@
 import clsx from 'clsx';
+import {CircleIcon} from '@/app/ui/icons';
 
 // Loading animation
 const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_3s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
 
-export function SkeletonRow(props: {width: string; height: string}) {
-  const {width, height} = props;
+export function SkeletonRow(props: {
+  width: string;
+  height: string;
+  color?: string;
+}) {
+  const {width, height, color = 'bg-slate-200'} = props;
 
-  return <div className={`bg-slate-200 ${width} ${height} rounded-md`} />;
+  return <div className={`${color} ${width} ${height} rounded-md`} />;
+}
+
+export function SkeletonListItem(props: {
+  width: string;
+  height: string;
+  color?: string;
+}) {
+  const {width, height, color = 'bg-slate-200'} = props;
+
+  return (
+    <div className='ml-7 relative'>
+      <CircleIcon
+        height={8}
+        className='absolute -start-[1.65rem] top-2 text-slate-500/90'
+        fill='currentColor'
+      />
+      <div className={`${color} ${width} ${height} rounded-md`} />
+    </div>
+  );
 }
 
 export function SkeletonAvatar(props: {diameter: number}) {
@@ -80,5 +104,42 @@ export function FullDashboardSkeleton() {
       <HeaderSkeleton />
       <DashboardSkeleton />
     </>
+  );
+}
+
+export function PatchNoteSkeleton() {
+  return (
+    <div
+      className={clsx(shimmer, 'relative flex flex-col gap-y-3 px-6 sm:px-20')}
+    >
+      <div className='bg-slate-50 flex flex-col gap-y-3 p-6 sm:px-20 rounded-lg'>
+        <div className='flex justify-between items-center mb-3'>
+          <SkeletonRow width='w-45' height='h-6' />
+          <SkeletonRow width='w-18' height='h-5' />
+        </div>
+        <SkeletonRow width='w-30' height='h-5' />
+        <SkeletonListItem width='w-[12rem]' height='h-5' />
+        <SkeletonListItem width='w-[15rem]' height='h-5' />
+        <SkeletonRow width='w-45' height='h-5' />
+        <SkeletonListItem width='w-[13rem]' height='h-5' />
+      </div>
+    </div>
+  );
+}
+
+export function LoadingSkeleton() {
+  return (
+    <div className='flex flex-col gap-6 mt-4'>
+      <div
+        className={clsx(
+          shimmer,
+          'relative flex flex-col gap-y-3 px-6 sm:px-20',
+        )}
+      >
+        <SkeletonRow color='bg-slate-100' width='w-30' height='h-6' />
+      </div>
+      <PatchNoteSkeleton />
+      <PatchNoteSkeleton />
+    </div>
   );
 }
